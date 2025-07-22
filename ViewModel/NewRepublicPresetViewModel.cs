e5264b02502fce898d2fc3992965e99f4ee80ad4
R4I0N <88613106+R4I0N@users.noreply.github.com>
2025-07-22 20:30:24 +0200

    New NR Categories + Overlay shown with checkbox

--- ViewModel/NewRepublicPresetViewModel.cs
+++ ViewModel/NewRepublicPresetViewModel.cs
@@ -4,19 +4,39 @@
     {
         public override string DisplayName => "New Republic Preset";
 
-        private bool _hireAdmiral;
+        private bool _hireSupCom;
+        private bool _hireHighCom;
+        private bool _hireOfficer;
         private bool _retireAdmiral;
         private bool _hireGeneral;
         private bool _retireGeneral;
         private bool _hireJedi;
         private bool _retireJedi;
 
-        public bool HireAdmiralNR
+        public bool HireSupComNR
         {
-            get => _hireAdmiral;
+            get => _hireSupCom;
             set
             {
-                _hireAdmiral = value;
+                _hireSupCom = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool HireHighComNR
+        {
+            get => _hireHighCom;
+            set
+            {
+                _hireHighCom = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool HireFleetOfficerNR
+        {
+            get => _hireOfficer;
+            set
+            {
+                _hireOfficer = value;
                 OnPropertyChanged();
             }
         }
